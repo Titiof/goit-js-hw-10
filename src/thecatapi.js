f4fe369e7f50b7be94caea056be6e78d2312a045
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 18:10:19 +0300

    Create thecatapi.js

---
+++ src/thecatapi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = 'api_key=live_KO7KqNftaOtiC6eCHgfVQMPc8HY8HnhfbJQNhZ6ZaXmo3RarbAcSMcK1xbuYVeIo';
+
+export function fetchBreeds() {
+    const url = 'https://api.thecatapi.com/v1/breeds';
+
+    return axios.get(url).then(response => response.data);
+}
+
+export function fetchCatByBreed(breedId) {
+    const baseUrl = 'https://api.thecatapi.com/v1/images/search';
+    const PARAMS = new URLSearchParams({ breed_ids: breedId });
+    const url = `${baseUrl}?${PARAMS}`;
+
+    return axios.get(url).then(response => response.data);
+}
